ae3f38b47e1ed696cefafc63245ea98aab70268e
Meenakshi Sundaram V <sundaram.viswanathan@ecyber.com>
2019-04-23 15:41:38 +0530

    Add repro

--- ChakraCoreRepro/Program.cs
+++ ChakraCoreRepro/Program.cs
@@ -1,12 +1,80 @@
-﻿using System;
+﻿using ChakraCore.NET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace ChakraCoreRepro
+namespace SegFaultRepro
 {
     class Program
     {
+        void Run()
+        {
+            ChakraRuntime runtime = null;
+            ChakraContext context = null;
+            string code = "";
+
+            try
+            {
+                runtime = ChakraRuntime.Create();
+                context = runtime.CreateContext(true);
+
+                // Expose .net objects to runtime
+                var service = context.ServiceNode.GetService<IJSValueConverterService>();
+
+                service.RegisterProxyConverter<Program>(
+                (binding, instance, node) =>
+                {
+                    binding.SetMethod<string>("echo", (msg) => { Console.WriteLine(msg); });
+                });
+
+                context.GlobalObject.WriteProperty("runtime", new Program());
+
+                // Load the code and run
+                using (Stream stream = File.OpenRead(@"app.js"))
+                {
+                    using (StreamReader streamReader = new StreamReader(stream))
+                    {
+                        code = streamReader.ReadToEnd();
+                    }
+                }
+
+                context.RunScript(code);
+            }
+            catch (Exception exp)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} {1}", DateTime.UtcNow.ToString("o"), exp.Message);
+                Console.ResetColor();
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var allTasks = new List<Task>();
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    var task = Task.Run(() =>
+                    {
+                        var program = new Program();
+                        program.Run();
+                    });
+
+                    allTasks.Add(task);
+                }
+
+                Task.WaitAll(allTasks.ToArray());
+                Console.WriteLine("All tasks done");
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
---
+++ ChakraCoreRepro/app.js
@@ -0,0 +1,29 @@
+﻿function asyncTask(ms) {
+    return new Promise((res, rej) => {
+        if (ms > 0) {
+            delay(ms);
+            res();
+            return;
+        }
+
+        rej("Delay should be greater than 0");
+    });
+}
+
+function delay(ms) {
+    let last = (new Date()).getTime();
+    while (true) {
+        let now = (new Date()).getTime();
+        if (now - last >= ms) {
+            return;
+        }
+    }
+}
+
+runtime.echo('Started execution @ ' + new Date().toISOString());
+
+asyncTask(100).then(() => {
+    runtime.echo('asyncTask done @ ' + new Date().toISOString())
+}).catch(err => {
+    runtime.echo('asyncTask done with error ' + err + ' @ ' + new Date().toISOString());
+}); 
